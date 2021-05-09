ec14a9a36ddd4cc03c4c93eb4b2d4c7aaacbd0e6
Ankit <ankitthakur50aa@gmail.com>
2021-05-09 21:23:25 +0530

    add no.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { scryRenderedDOMComponentsWithClass } from 'react-dom/test-utils';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+
+const App=()=>{ 
+  const [Fullname,setFullname]= useState({
+    fname:'',
+    pass:'',
+    email:'',
+    phone:'',
+  }) ;
+ 
+  
+  
+  const InputEvent=(e)=>{ 
+  console.log(e.target.value); 
+  const {value,name}=e.target;
+  
+
+  setFullname((preValue)=>{
+    return{
+      ...preValue,
+      [name]:value,
+    }
+
+   
+         
+  })
+  };
+  
+  const onSubmit=(e)=>{ 
+    e.preventDefault();
+  
+  }
+ 
+  
+ return (<>
+  <div> 
+  <form onSubmit={onSubmit} >
+    <h1 style={{color:'blue'}} >Hello {Fullname.fname} {Fullname.pass}  {Fullname.email}  {Fullname.phone} </h1> <br></br> 
+    <h2>6268</h2>
+    <input type='text' placeholder='Enter Your Name' onChange={InputEvent} value={Fullname.fname} name='fname' /> <br></br> <br></br>
+    <input type='text' placeholder='Enter Your password' onChange={InputEvent} value={Fullname.pass} name='pass' /> <br></br> <br></br>
+    <input type='text' placeholder='Enter Your email' onChange={InputEvent} value={Fullname.email} name='email' /> <br></br> <br></br>
+    <input type='text' placeholder='Enter Your phone no.' onChange={InputEvent} value={Fullname.phone} name='phone' /> <br></br> <br></br>
+    
+    <button  type='submit'>Click Me</button> 
+    </form>
+  </div>
+</>
+ );
+} 
 
 export default App;
